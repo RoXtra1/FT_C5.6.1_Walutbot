446a88e561c00dd4e4658f003372c6b2c4992955
RoXtra <skornyakovartem@mail.ru>
2024-02-09 17:47:41 +0300

    Update config.py
    скрыл токены бота и АПИ

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 payload = {} # нужно для работы апи
-TKN = "5388159569:AAF5bJz9T5kB48Ayc6FaHrgmbMTKMrEbM2E" # пароль от бота
-headers= {"apikey": "x55pb7FGx7mik2l9c0NlNVVOa4tP6egV"} # ключ от апи
+TKN = "***" # токен ключ от бота
+headers= {"apikey": "***"} # ключ от апи
 
-keys = {'евро':'EUR', # доступные тикеры. можно дополнять
+keys = {'евро':'EUR', # доступные валюты. можно дополнять
         'доллар':'USD',
         'рубль':'RUB'}
